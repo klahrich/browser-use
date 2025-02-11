d9e4231d73a81bf1ea89b61880cbf0f9a5ee34b9
Vladimir Dolgopolov <vladimir.i.dolgopolov@gmail.com>
2025-02-11 10:53:52 -0500

    remove commented out action

--- browser_use/controller/service.py
+++ browser_use/controller/service.py
@@ -169,13 +169,6 @@ class Controller:
 			logger.info(msg)
 			return ActionResult(extracted_content=msg)
 
-		# TODO: This needs to be overwritten in order to have contorl over exiting the application
-		# Leaving it here as an example, but need to implement unique one in your controller
-
-		'''@self.registry.action('Complete task', param_model=DoneAction)
-		async def done(params: DoneAction):
-			return ActionResult(is_done=True, extracted_content=params.text)'''
-
 		@self.registry.action(
 			'Scroll down the page by pixel amount - if no amount is specified, scroll down one page',
 			param_model=ScrollAction,
